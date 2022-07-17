0f64393b2fb2243c3317601ff879aff94c17e1a1
oshikawatkm <tkm05oshikawa12@gmail.com>
2022-07-17 16:31:12 +0900

    setup db

---
+++ HotelListingAPI.VSCode/Data/HotelListingDBContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+  public class HotelListingDbContext : DbContext
+  {
+
+    public HotelListingDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+  }
+}
